30654050ec1ed339b35872afdaa05824fa31fc89
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    AOKP custom system animations (1/2)
    - Work by AOKP team and ported by @cristianomatos @crdroidandroid
    
    Change-Id: Ibeaa0f369279bfa94f2547c423483f7eff80bcb8
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    
    AOKP animations: Add an entry for TRANSIT_TASK_OPEN_BEHIND [1/2]
    
    Change-Id: I306c932c79d32e727f15fe1645ffa723d3169bec
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Fix animation duration for custom animations
    
    Add missing helper

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3775,6 +3775,25 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_LOCK_SCREEN = "double_tap_sleep_lock_screen";
 
+        /**  
+         * AOKP Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+
         /**
          * Whether to allow one finger quick settings expansion on the left or right side of the statusbar.
          * @hide
