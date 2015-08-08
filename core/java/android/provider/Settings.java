d6f2474c9277ac70753e4541d70a1e3b9eaaca94
ghbhaha <ghbhaha@gmail.com>
2015-08-09 01:23:33 +0800

    [1/2]frameworks/base: Add three-fingers-swipe to screenshot The feature is ported from Oppo ColorOS. With the options on, users can swipe down on the screen to have a screenshot.
    original smali port by: wuxianlin
    
    Change-Id: I667f8e8b351dd9cb975d16dd6ffae53e79b61b03

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4348,6 +4348,12 @@ public final class Settings {
         public static final String QS_SMART_PULLDOWN = "qs_smart_pulldown";
 
         /**
+         * Three Finger Gesture from Oppo
+         * @hide
+         */
+        public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
