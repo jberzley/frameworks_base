32387e66d8433f2c344b23f01d9c572df1f29352
bigrushdog <randall.rushing@gmail.com>
2015-11-13 00:50:14 -0800

    Statusbar: Gracefully adapt 3Minit Battery hook for AOSP
    3Minit Battery is a free or paid third party statusbar
    battery icon solution. This commit builds upon the
    initial SystemUI hook source release by Gary Harrington.
    
    We are able to significantly optimize the original code
    because we are not constrained by the "one-size-fits-all"
    conditions the original code must meet. At the same time,
    we fully maintain the integrity of the UX intended by
    the original code.
    
    Of note is the introduction of a controller class. This
    class takes over settings and state management as well
    as handles receiver events. The controller class also
    monitors package changes and updates feature visibility
    accordingly if a needed resource is added or removed.
    
    Upon initialization, if no feature resources are
    detected, all resources are freed.
    
    ** Changes for N **
    
    There is no longer a batter view in the statusbar header
    so don't pass as constructor arg and adapt in controller
    class.
    
    Change-Id: I0254bfa0becfbf6e83e9d0547abda8a77047ab1e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mCenterClock, mDesoLogo, mNetworkTraffic;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mCenterClock, mDesoLogo, mNetworkTraffic, mMinitBattery;
 
     private Animator mCurrentAnimation;
 
@@ -52,6 +52,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mCenterClock = mView.findViewById(R.id.center_clock);
         mDesoLogo = mView.findViewById(R.id.deso_logo);
         mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
+        mMinitBattery = mView.findViewById(R.id.minitBattery);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -98,8 +99,9 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mNetworkTraffic, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC),
-                    animateTransitionTo(mCenterClock, newAlphaBC),
                     animateTransitionTo(mDesoLogo, newAlphaBC)
+                    animateTransitionTo(mCenterClock, newAlphaBC),
+                    animateTransitionTo(mMinitBattery, newAlphaBC)
                     );
             if (isLightsOut(mode)) {
                 anims.setDuration(LIGHTS_OUT_DURATION);
@@ -115,6 +117,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mClock.setAlpha(newAlphaBC);
             mCenterClock.setAlpha(newAlphaBC);
             mDesoLogo.setAlpha(newAlphaBC);
+            mMinitBattery.setAlpha(newAlphaBC);
         }
     }
 }
