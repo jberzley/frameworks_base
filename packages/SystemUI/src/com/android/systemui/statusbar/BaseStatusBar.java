4f28e6ce8e061e25c0d2b55cae3a1f7dee84f0f3
Brett Rogers <brettrogers11@gmail.com>
2016-10-07 02:31:16 +0200

    Status bar notification ticker
    Change-Id: I29c2afc74376fa220ee88c7cec07d35d56c14680

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2427,8 +2427,10 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
     }
 
+    protected abstract void haltTicker();
     protected abstract void setAreThereNotifications();
     protected abstract void updateNotifications();
+    protected abstract void tick(StatusBarNotification n, boolean firstTime);
     public abstract boolean shouldDisableNavbarGestures();
 
     public abstract void addNotification(StatusBarNotification notification,
@@ -2450,6 +2452,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         Notification n = notification.getNotification();
         mNotificationData.updateRanking(ranking);
+
+        boolean updateTicker = n.tickerText != null
+                && !TextUtils.equals(n.tickerText,
+                entry.notification.getNotification().tickerText);
 
         boolean applyInPlace;
         try {
@@ -2523,11 +2529,15 @@ public abstract class BaseStatusBar extends SystemUI implements
             mStackScroller.snapViewIfNeeded(entry.row);
         }
 
-        if (DEBUG) {
-            // Is this for you?
-            boolean isForCurrentUser = isNotificationForCurrentProfiles(notification);
-            Log.d(TAG, "notification is " + (isForCurrentUser ? "" : "not ") + "for you");
-        }
+        // Is this for you?
+        boolean isForCurrentUser = isNotificationForCurrentProfiles(notification);
+        Log.d(TAG, "notification is " + (isForCurrentUser ? "" : "not ") + "for you");
+
+        // Restart the ticker if it's still running
+        if (updateTicker && isForCurrentUser) {
+            haltTicker();
+            tick(notification, false);
+          }
 
         setAreThereNotifications();
     }
