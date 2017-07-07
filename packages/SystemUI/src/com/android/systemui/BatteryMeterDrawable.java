9111ed319a37f9cef71356cb168087ecdd42efc4
Snuzzo <jrracinfan@gmail.com>
2017-07-07 14:52:28 -0400

    Revert "SystemUI: Implement hidden and text battery styles"
    This reverts commit a29ea6a264c44d7b26c9556c288e0aa13a870a56.

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -64,10 +64,6 @@ public class BatteryMeterDrawable extends Drawable implements
 
     // Values for the different battery styles
     public static final int BATTERY_STYLE_PORTRAIT  = 0;
-    public static final int BATTERY_STYLE_CIRCLE    = 2;
-    public static final int BATTERY_STYLE_HIDDEN    = 4;
-    public static final int BATTERY_STYLE_LANDSCAPE = 5;
-    public static final int BATTERY_STYLE_TEXT      = 6;
 
     private final int[] mColors;
     private final int mIntrinsicWidth;
